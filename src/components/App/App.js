11e62c8197df0ca844fc4b9f5f793a7bc68edfbf
Maria DelSignore <maria.ad27@gmail.com>
2021-11-09 11:07:46 -0700

    Link up network request

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,16 @@
+import { useEffect, useState } from 'react';
 import { getData } from '../../utils/apiCalls';
 import MainContent from '../MainContent/MainContent';
 import NavBar from '../NavBar/NavBar';
 import './App.css';
 
 const App = () => {
+  const [articles, setArticles] = useState()
+
+  useEffect(() => {
+    getData('arts')
+  }, [])
+
   return (
     <div className="App">
       <p>New York Times News Reader</p>
--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,8 +1,10 @@
-const getData = (category) => {
+export const getData = (category) => {
   fetch(`https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=5EdWhsnyQbSNLLfvv9XAl96fXPastXTg`)
+    .then(res => res.json())
+    .then(data => console.log('data: ', data.results))
 }
 
-export default getData;
+
 
 
 
