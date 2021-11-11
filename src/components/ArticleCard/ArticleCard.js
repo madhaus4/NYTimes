0cc7621d2f7fac9c642e9b5a1f70b6a143c7f809
Maria DelSignore <maria.ad27@gmail.com>
2021-11-11 09:19:20 -0700

    Render article cards

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -1,12 +1,14 @@
 import './ArticleCard.css';
 
-const ArticleCard = ({ title, date }) => {
+const ArticleCard = ({ title, date, abstract, multimedia }) => {
 
   return (
     <article className='article-card'>
       {/* <p>I am the ARTICLE CARD</p> */}
+      <img src={multimedia} alt='' />
       <p>{title}</p>
       <p>{date}</p>
+      <p>{abstract}</p>
     </article>
   )
 }
