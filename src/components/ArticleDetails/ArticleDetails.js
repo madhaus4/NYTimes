17d9a184b55dd1ee4e668734cc698c1a10af1548
Maria DelSignore <maria.ad27@gmail.com>
2021-11-11 11:10:19 -0700

    Add conditional render to img and caption

--- src/components/ArticleDetails/ArticleDetails.js
+++ src/components/ArticleDetails/ArticleDetails.js
@@ -7,8 +7,8 @@ const ArticleDetails = ({ currentArticle }) => {
   return (
     <section className='article-details-container'>
       <p> I am the ARTICLE DETAILS</p>
-      <img src={currentArticle.multimedia[0].url} alt='' />
-      <p>IMG CAPTION: {currentArticle.multimedia[0].caption}</p>
+      {currentArticle.multimedia !== undefined && <img src={currentArticle.multimedia[0].url} alt='' />}
+      {currentArticle.multimedia !== undefined && <p>IMG CAPTION: {currentArticle.multimedia[0].caption}</p>}
       <p>TITLE: {currentArticle.title}</p>
       <p>DATE: {currentArticle.published_date}</p>
       <p>ABSTRACT: {currentArticle.abstract}</p>
