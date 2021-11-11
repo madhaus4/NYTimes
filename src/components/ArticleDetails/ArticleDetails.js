465f4e72326738266578490ae498217b7d8c4b9a
Maria DelSignore <maria.ad27@gmail.com>
2021-11-11 10:51:43 -0700

    Add data points to render in detailed view

--- src/components/ArticleDetails/ArticleDetails.js
+++ src/components/ArticleDetails/ArticleDetails.js
@@ -2,12 +2,18 @@ import './ArticleDetails.css';
 
 const ArticleDetails = ({ currentArticle }) => {
 
+  console.log('currentArticle: ', currentArticle)
+
   return (
     <section className='article-details-container'>
       <p> I am the ARTICLE DETAILS</p>
-      {/* <img src={} alt='' /> */}
-      <p>{currentArticle.title}</p>
-      <p>{currentArticle.date}</p>
+      <img src={currentArticle.multimedia[0].url} alt='' />
+      <p>IMG CAPTION: {currentArticle.multimedia[0].caption}</p>
+      <p>TITLE: {currentArticle.title}</p>
+      <p>DATE: {currentArticle.published_date}</p>
+      <p>ABSTRACT: {currentArticle.abstract}</p>
+      <p>BYLINE: {currentArticle.by_line}</p>
+      <a href={currentArticle.short_url}>VIEW FULL ARTICLE HERE</a>
     </section>
   )
 }
