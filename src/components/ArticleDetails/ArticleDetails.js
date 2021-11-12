06898df96f3c9680978c204a0650bb8f232c1067
Maria DelSignore <maria.ad27@gmail.com>
2021-11-11 21:51:25 -0700

    Adjust nav bar spacing

--- src/components/ArticleDetails/ArticleDetails.js
+++ src/components/ArticleDetails/ArticleDetails.js
@@ -6,13 +6,13 @@ const ArticleDetails = ({ currentArticle }) => {
     <>
 
     {!currentArticle.title && <section className='article-details-container'>
-      <h2>click on an article to view details</h2>
+      <h1>click on an article to view details</h1>
     </section>}
 
     {currentArticle.title && <section className='article-details-container'>
+      <h2>{currentArticle.title}</h2>
       {currentArticle.multimedia !== undefined && <img src={currentArticle.multimedia[0].url} alt='' />}
       {currentArticle.multimedia !== undefined && <p>IMG CAPTION: {currentArticle.multimedia[0].caption}</p>}
-      <p>TITLE: {currentArticle.title}</p>
       <p>DATE: {currentArticle.published_date}</p>
       <p>ABSTRACT: {currentArticle.abstract}</p>
       <p>BYLINE: {currentArticle.by_line}</p>
